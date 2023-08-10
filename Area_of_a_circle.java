930941340db0a101e7f135cbe367f9f337835555
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-10 11:42:39 +0530

    import java.util.*; public class Suresh{     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int r;         double area;         r = sc.nextInt();         area = 3.14*r*r;         System.out.printf("%.2f",area);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int r;
+        double area;
+        r = sc.nextInt();
+        area = 3.14*r*r;
+        System.out.printf("%.2f",area);
+    }
+}
