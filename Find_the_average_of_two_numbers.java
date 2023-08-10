5f6ed528898b15018c97b2a8db67879623e5ac98
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-10 11:48:33 +0530

    import java.util.*; public class Suresh{     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         int a,b;         double c;         a = sc.nextInt();         b = sc.nextInt();         c=(double)(a+b)/2;         System.out.printf("%.4f",c);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        int a,b;
+        double c;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c=(double)(a+b)/2;
+        System.out.printf("%.4f",c);
+    }
+}
