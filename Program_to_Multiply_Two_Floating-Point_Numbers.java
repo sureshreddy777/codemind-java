466f02b05d8913848b21adebfe5dcae4f177f3be
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-10 12:00:59 +0530

    import java.util.*; public class Suresh{     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         float a,b;         double c;         a=sc.nextFloat();         b=sc.nextFloat();         c=a*b;         System.out.printf("%.2f",c);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        float a,b;
+        double c;
+        a=sc.nextFloat();
+        b=sc.nextFloat();
+        c=a*b;
+        System.out.printf("%.2f",c);
+    }
+}
