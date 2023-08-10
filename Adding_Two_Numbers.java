2e5e59a1966558213fa7f1351ed33bcd49940f85
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-10 18:22:56 +0530

    import java.util.*; public class Suresh{     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int a,b,c;         a = sc.nextInt();         b = sc.nextInt();         c=a+b;         System.out.printf("%d",c);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int a,b,c;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c=a+b;
+        System.out.printf("%d",c);
+    }
+}
