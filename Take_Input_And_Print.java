82e3e5129e0ecfa8823e8d250d985ff4653d775d
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-10 12:18:19 +0530

    import java.util.*; public class Suresh {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         String a;         a = sc.nextLine();         System.out.print(a);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Suresh
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        String a;
+        a = sc.nextLine();
+        System.out.print(a);
+    }
+}
