d2bb39ebe9ded6c089ad45a72625139f3ff5d8b3
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-10 18:42:53 +0530

    import java.util.*; public class Suresh {     public static void main(String args[])     {         Scanner sc = new Scanner (System.in);
            char chr = sc.next().charAt(0);  
            int asciiValue = chr;
            System.out.println(asciiValue);
        }
    }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Suresh
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner (System.in);
+        
+        char chr = sc.next().charAt(0);  
+        int asciiValue = chr;
+        System.out.println(asciiValue);
+    }
+}
