fe721509b32acd47a855763a6ac3650435db7052
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-13 13:37:28 +0530

    import java.util.*; public class Suresh {     public static void main(String args[])     {         Scanner md = new Scanner (System.in);         int n;         n = md.nextInt();         System.out.println(n%2==0 ?"Even":"Odd");
        }
    }

---
+++ Check_if_number_is_even_or_odd.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Suresh
+{
+    public static void main(String args[])
+    {
+        Scanner md = new Scanner (System.in);
+        int n;
+        n = md.nextInt();
+        System.out.println(n%2==0 ?"Even":"Odd");
+        
+    }
+}
