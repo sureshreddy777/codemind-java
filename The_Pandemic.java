c82d12214d93f162283bc7feb137994a2a6a4092
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-13 12:36:32 +0530

    import java.util.*; public class Suresh {     public static void main(String args[])     {         Scanner md = new Scanner (System.in);         int n;         n = md.nextInt();         if (n == 2)         {             System.out.println("1");         }         else if (n == 1)         {             System.out.println("0");         }         else         {             System.out.println("2");         }     } }

---
+++ The_Pandemic.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Suresh
+{
+    public static void main(String args[])
+    {
+        Scanner md = new Scanner (System.in);
+        int n;
+        n = md.nextInt();
+        if (n == 2)
+        {
+            System.out.println("1");
+        }
+        else if (n == 1)
+        {
+            System.out.println("0");
+        }
+        else
+        {
+            System.out.println("2");
+        }
+    }
+}
