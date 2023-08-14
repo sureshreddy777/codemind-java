d07a088d6dca3ff6a4131d721ffea14ab162e2ea
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-14 15:55:45 +0530

    import java.util.*; public class Suresh {     public static void main(String args[])     {         Scanner md = new Scanner (System.in);         char n;         n = md.nextLine().charAt(0);         if (n == 'A' || n == 'a' || n == 'E' ||n == 'e' ||n == 'I' ||n == 'i' ||n == 'O' ||n == 'o' ||n == 'U' ||n == 'u'){             System.out.println("Vowel");         }         else{             System.out.println("Consonant");         }     } }

---
+++ Program_to_check_Vowel_or_Consonant.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Suresh
+{
+    public static void main(String args[])
+    {
+        Scanner md = new Scanner (System.in);
+        char n;
+        n = md.nextLine().charAt(0);
+        if (n == 'A' || n == 'a' || n == 'E' ||n == 'e' ||n == 'I' ||n == 'i' ||n == 'O' ||n == 'o' ||n == 'U' ||n == 'u'){
+            System.out.println("Vowel");
+        }
+        else{
+            System.out.println("Consonant");
+        }
+    }
+}
