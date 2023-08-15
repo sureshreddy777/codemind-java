e511ddafcfa7303c854c2b94f73de778030c1310
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-15 18:59:13 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner (System.in);         int n;         n = md.nextInt();         int s = n*(n+1)/2;         System.out.println(s);     } }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String args[]){
+        Scanner md = new Scanner (System.in);
+        int n;
+        n = md.nextInt();
+        int s = n*(n+1)/2;
+        System.out.println(s);
+    }
+}
