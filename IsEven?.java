b3a6d6e10ea047e857b162f7e18ac952af8c2fe1
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-16 19:21:39 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner(System.in);         int n = md.nextInt();         if (n%2==0){             System.out.println("2");         }         else {             System.out.println("1");         }     } }

---
+++ IsEven?.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String args[]){
+        Scanner md = new Scanner(System.in);
+        int n = md.nextInt();
+        if (n%2==0){
+            System.out.println("2");
+        }
+        else {
+            System.out.println("1");
+        }
+    }
+}
