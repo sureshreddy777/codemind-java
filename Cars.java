143c66e16e07bdaa66d92ddbda16e5027ee418ab
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-18 20:06:59 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner (System.in);         int n = md.nextInt();         if (n%4 == 0){             System.out.println(n/4);         }         else{             System.out.println((n/4)+1);         }     } }

--- Cars.java
+++ Cars.java
@@ -1,30 +1,13 @@
 import java.util.*;
-public class Suresh
-{
-    public static void main(String args[])
-    {
-        Scanner sc = new Scanner (System.in);
-        int n1,n2,x;
-        n1 = sc.nextInt();
-        n2 = sc.nextInt();
-        x = sc.nextInt();
-        if (n1>n2)
-        {
-            System.out.println("-1");
+public class Suresh{
+    public static void main(String args[]){
+        Scanner md = new Scanner (System.in);
+        int n = md.nextInt();
+        if (n%4 == 0){
+            System.out.println(n/4);
         }
-        else 
-        {
-            int count = 1;
-            int a,b;
-            a = n1;
-            b = n2;
-            while (x+a>=b)
-            {
-                count++;
-                a+=n1;
-                b+=n2;
-            }
-            System.out.println(count);
+        else{
+            System.out.println((n/4)+1);
         }
     }
 }
