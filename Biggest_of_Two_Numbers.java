3ed3931d08c3684d49602a1a3097fc67748092fe
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-18 19:47:59 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner (System.in);         int a,b;         a = md.nextInt();         b = md.nextInt();         if (a>b){             System.out.println(a);         }else{             System.out.println(b);         }     } }

---
+++ Biggest_of_Two_Numbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String args[]){
+        Scanner md = new Scanner (System.in);
+        int a,b;
+        a = md.nextInt();
+        b = md.nextInt();
+        if (a>b){
+            System.out.println(a);
+        }else{
+            System.out.println(b);
+        }
+    }
+}
