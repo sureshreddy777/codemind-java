a0a86010dcf5896c8e3f02252851ff2d90a15095
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-18 19:42:18 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner (System.in);         int c = md.nextInt();         System.out.println((c>20)?"HOT":"COLD");     } }

---
+++ Climate.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String args[]){
+        Scanner md = new Scanner (System.in);
+        int c = md.nextInt();
+        System.out.println((c>20)?"HOT":"COLD");
+    }
+}
