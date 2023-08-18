b55830b86f62c1749352a5beb8aabad0a4edbf0b
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-18 12:10:14 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner (System.in);         int n = md.nextInt();         for (int i=1;i<=n;i++){             System.out.printf("%d ",i*3);         }     } }

---
+++ First_N_multiples_of_3.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String args[]){
+        Scanner md = new Scanner (System.in);
+        int n = md.nextInt();
+        for (int i=1;i<=n;i++){
+            System.out.printf("%d ",i*3);
+        }
+    }
+}
