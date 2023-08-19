31fc0bdde5267fca17dc0cbb245bcd2097c0b616
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-19 19:25:36 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner (System.in);         int n = md.nextInt();         int sum = n*(n+1)/2;         System.out.println(sum);     } }

---
+++ Sum_of_first_N_natural_numbers.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String args[]){
+        Scanner md = new Scanner (System.in);
+        int n = md.nextInt();
+        int sum = n*(n+1)/2;
+        System.out.println(sum);
+    }
+}
