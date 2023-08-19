df527699ff2f7c73b54db1eeebeabc1cbd58c06b
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-19 19:43:40 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner (System.in);         int n;         n = md.nextInt();         for (int i=1;i<=n;i++){             System.out.printf(i+" ");         }
        }
    }

---
+++ 1_to_n.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String args[]){
+        Scanner md = new Scanner (System.in);
+        int n;
+        n = md.nextInt();
+        for (int i=1;i<=n;i++){
+            System.out.printf(i+" ");
+        }
+        
+    }
+}
