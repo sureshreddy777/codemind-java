cbdc0163119b5f07f8bf16e324624f7f5556c296
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-19 21:09:08 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner (System.in);         int n = md.nextInt();         int[] a = new int[n];         int sum = 0;         for (int i=0;i<n;i++){             a[i] = md.nextInt();         }         for (int i=0;i<n;i++){             if (a[i]%2 != 0){                 sum+=a[i];             }         }         System.out.printf("%d",sum);     } }

---
+++ Odd_Sum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String args[]){
+        Scanner md = new Scanner (System.in);
+        int n = md.nextInt();
+        int[] a = new int[n];
+        int sum = 0;
+        for (int i=0;i<n;i++){
+            a[i] = md.nextInt();
+        }
+        for (int i=0;i<n;i++){
+            if (a[i]%2 != 0){
+                sum+=a[i];
+            }
+        }
+        System.out.printf("%d",sum);
+    }
+}
