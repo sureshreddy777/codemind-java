cbea11a8cfe96d21f2fbfd8225f0ee188020366c
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-19 21:15:42 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner (System.in);         int n= md.nextInt();         int[] a = new int[n];         for (int i=0;i<n;i++){             a[i] = md.nextInt();         }         int sum=0;         for (int i=0;i<n;i++){             sum+=a[i];         }         System.out.print(sum);     } }

--- Sum_of_Array.java
+++ Sum_of_Array.java
@@ -2,15 +2,15 @@ import java.util.*;
 public class Suresh{
     public static void main(String args[]){
         Scanner md = new Scanner (System.in);
-        int n = md.nextInt();
-        int sum = 0;
+        int n= md.nextInt();
         int[] a = new int[n];
         for (int i=0;i<n;i++){
             a[i] = md.nextInt();
         }
+        int sum=0;
         for (int i=0;i<n;i++){
             sum+=a[i];
         }
-        System.out.println(sum);
+        System.out.print(sum);
     }
 }
