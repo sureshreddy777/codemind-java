93ced57f631aac6f43b72198fb78c6fc00be1227
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-19 19:42:31 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner (System.in);         int a,b;         a = md.nextInt();         b = md.nextInt();         for (int i=a;i<=b;i++){             System.out.print(i+" ");         }
        }
    }

---
+++ A_to_B.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String args[]){
+        Scanner md = new Scanner (System.in);
+        int a,b;
+        a = md.nextInt();
+        b = md.nextInt();
+        for (int i=a;i<=b;i++){
+            System.out.print(i+" ");
+        }
+        
+    }
+}
