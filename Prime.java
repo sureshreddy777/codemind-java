386a703d26a62104f08160e0444278c21ee97685
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-20 12:21:23 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner (System.in);         int n = md.nextInt();         int count = 0;         for (int i=2;i<n;i++){             if (n%i == 0){                 count++;             }         }         if (count ==0){             System.out.println("Prime");         }         else{             System.out.println("Not Prime");         }     } }

--- Prime.java
+++ Prime.java
@@ -2,14 +2,14 @@ import java.util.*;
 public class Suresh{
     public static void main(String args[]){
         Scanner md = new Scanner (System.in);
-        int n= md.nextInt();
-        int flag = 0;
+        int n = md.nextInt();
+        int count = 0;
         for (int i=2;i<n;i++){
-            if (n%i ==0){
-                flag = 1;
+            if (n%i == 0){
+                count++;
             }
         }
-        if (flag ==0){
+        if (count ==0){
             System.out.println("Prime");
         }
         else{
