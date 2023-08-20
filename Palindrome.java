dd235499d78cdcd7d784ac1c492d19b498b65d0f
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-20 12:16:27 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner (System.in);         int n = md.nextInt();         int t,rem,r =0;         t = n;         while (n!=0){             rem = n%10;             r = r*10+rem;             n = n/10;         }         if (t == r){             System.out.println("Palindrome");         }         else{             System.out.println("Not Palindrome");         }     } }

---
+++ Palindrome.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String args[]){
+        Scanner md = new Scanner (System.in);
+        int n = md.nextInt();
+        int t,rem,r =0;
+        t = n;
+        while (n!=0){
+            rem = n%10;
+            r = r*10+rem;
+            n = n/10;
+        }
+        if (t == r){
+            System.out.println("Palindrome");
+        }
+        else{
+            System.out.println("Not Palindrome");
+        }
+    }
+}
