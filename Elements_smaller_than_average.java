985fe828c3963592e6beaf1fe7730c84c1a61702
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-20 11:45:17 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner (System.in);         int n = md.nextInt();         int[] a = new int[n];         for (int i=0;i<n;i++){             a[i] = md.nextInt();         }         int sum = 0;         for (int i=0;i<n;i++){             sum+=a[i];         }         int avg = sum/n;         int count = 0;         for (int i=0;i<n;i++){             if (a[i]<=avg){                 count++;             }         }         System.out.println(count);     } }

---
+++ Elements_smaller_than_average.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String args[]){
+        Scanner md = new Scanner (System.in);
+        int n = md.nextInt();
+        int[] a = new int[n];
+        for (int i=0;i<n;i++){
+            a[i] = md.nextInt();
+        }
+        int sum = 0;
+        for (int i=0;i<n;i++){
+            sum+=a[i];
+        }
+        int avg = sum/n;
+        int count = 0;
+        for (int i=0;i<n;i++){
+            if (a[i]<=avg){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
