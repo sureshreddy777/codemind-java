8de264db70bd552de540a903e104bf09a46613c0
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-21 16:10:15 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner (System.in);         int n = md.nextInt();         int[] a = new int[n];         for (int i=0;i<n;i++){             a[i] = md.nextInt();         }         int z = md.nextInt();         int count  = 0;         for (int i=0;i<n;i++){             if (a[i] == z){                 count++;             }         }         System.out.println(count);     } }

---
+++ Count_of_given_element_in_an_array.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String args[]){
+        Scanner md = new Scanner (System.in);
+        int n = md.nextInt();
+        int[] a = new int[n];
+        for (int i=0;i<n;i++){
+            a[i] = md.nextInt();
+        }
+        int z = md.nextInt();
+        int count  = 0;
+        for (int i=0;i<n;i++){
+            if (a[i] == z){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
