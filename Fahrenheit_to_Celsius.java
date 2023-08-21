30667bbb99bbca7b69ee4c8337e77eeff952614b
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-21 16:36:41 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner (System.in);         int n = md.nextInt();         double c = (n-32)*0.5555555555555556;         System.out.printf("%.2f",c);     } }

---
+++ Fahrenheit_to_Celsius.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String args[]){
+        Scanner md = new Scanner (System.in);
+        int n = md.nextInt();
+        double c = (n-32)*0.5555555555555556;
+        System.out.printf("%.2f",c);
+    }
+}
