9aff972bb4f16a923b20024834d834687ee8476e
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-23 20:05:59 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner (System.in);         int n = md.nextInt();         int a,b;         for (int i=0;i<n;i++){             a = md.nextInt();             b = md.nextInt();             System.out.println(a+b);         }     } }

---
+++ sum_of_the_given_numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String args[]){
+        Scanner md = new Scanner (System.in);
+        int n = md.nextInt();
+        int a,b;
+        for (int i=0;i<n;i++){
+            a = md.nextInt();
+            b = md.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
