198eb5584991eb27736c6eaf19f3c07627fcad38
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-23 19:42:19 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner (System.in);         int x,y,z;         x = md.nextInt();         y = md.nextInt();         z = md.nextInt();         if (x>50 && y>60 && z>100){             System.out.println("10");         }         else if (x>50 && y>60 && z<100){             System.out.println("9");         }         else if (x<50 && y>60 && z>100){             System.out.println("8");         }         else if (x>50 && y<60 && z>100){             System.out.println("7");         }         else if (x>50 || y>60 || z>100){             System.out.println("6");         }         else{             System.out.println("5");         }     } }

--- Grades_of_Rides.java
+++ Grades_of_Rides.java
@@ -1,35 +1,27 @@
 import java.util.*;
-public class Suresh
-{
-    public static void main(String args[])
-    {
+public class Suresh{
+    public static void main(String args[]){
         Scanner md = new Scanner (System.in);
-        int hf,sf,sef;
-        hf = md.nextInt();
-        sf = md.nextInt();
-        sef = md.nextInt();
-        if (hf>50 && sf>60 && sef>100)
-        {
+        int x,y,z;
+        x = md.nextInt();
+        y = md.nextInt();
+        z = md.nextInt();
+        if (x>50 && y>60 && z>100){
             System.out.println("10");
         }
-        else if (hf>50 && sf>60 && sef<100)
-        {
+        else if (x>50 && y>60 && z<100){
             System.out.println("9");
         }
-        else if (sef>100 && sf>60 && hf<50)
-        {
+        else if (x<50 && y>60 && z>100){
             System.out.println("8");
         }
-        else if (hf>50 && sef>100 && sf<60)
-        {
+        else if (x>50 && y<60 && z>100){
             System.out.println("7");
         }
-        else if (hf>50 || sf>60 || sef>100)
-        {
+        else if (x>50 || y>60 || z>100){
             System.out.println("6");
         }
-        else
-        {
+        else{
             System.out.println("5");
         }
     }
