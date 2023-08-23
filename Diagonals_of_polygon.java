27730f80d2c2fffd89e0b36e3e2216de708efcdb
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-23 19:05:48 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner (System.in);         int n = md.nextInt();         int s = n*(n-3)/2;         System.out.println(s);     } }

---
+++ Diagonals_of_polygon.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String args[]){
+        Scanner md = new Scanner (System.in);
+        int n = md.nextInt();
+        int s = n*(n-3)/2;
+        System.out.println(s);
+    }
+}
