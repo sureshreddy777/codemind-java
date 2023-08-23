e2669666e33506741ff2e5a6330c4850a9536e44
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-23 21:24:43 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner (System.in);         int n = md.nextInt();         double h = n*2.54;         System.out.printf("%.2f",h);     } }

---
+++ inches_to_centimeters.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String args[]){
+        Scanner md = new Scanner (System.in);
+        int n = md.nextInt();
+        double h = n*2.54;
+        System.out.printf("%.2f",h);
+    }
+}
