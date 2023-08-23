333759b0a04dc91d8e7894ca62fe4613a63c2816
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-23 18:58:59 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner (System.in);         int p,t,r;         p = md.nextInt();         t = md.nextInt();         r = md.nextInt();         int si = p*t*r/100;         System.out.println(si);     } }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String args[]){
+        Scanner md = new Scanner (System.in);
+        int p,t,r;
+        p = md.nextInt();
+        t = md.nextInt();
+        r = md.nextInt();
+        int si = p*t*r/100;
+        System.out.println(si);
+    }
+}
