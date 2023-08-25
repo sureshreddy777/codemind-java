aacf05b345eb3012c630a7bbfb74d6b8f483295f
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-25 10:06:23 +0530

    import java.util.*; public class Suresh{     public static void main(String arg[]){         Scanner md = new Scanner (System.in);         int n = md.nextInt();         int[] a = new int[n];         for (int i=0;i<n;i++){             a[i] = md.nextInt();         }         int s = 0;         for (int i=0;i<n;i++){             if (a[i]%2 == 0){                 s = i;             }         }         System.out.println(s);     } }

---
+++ Last_even_element_index.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String arg[]){
+        Scanner md = new Scanner (System.in);
+        int n = md.nextInt();
+        int[] a = new int[n];
+        for (int i=0;i<n;i++){
+            a[i] = md.nextInt();
+        }
+        int s = 0;
+        for (int i=0;i<n;i++){
+            if (a[i]%2 == 0){
+                s = i;
+            }
+        }
+        System.out.println(s);
+    } 
+}
