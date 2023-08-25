fa3474aad7ba4d335dcd842da9a8210f3fb9f740
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-25 09:48:30 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner (System.in);         int n = md.nextInt();         int[] a = new int[n];         for (int i=0;i<n;i++){             a[i] = md.nextInt();         }         Arrays.sort(a);         System.out.println(a[a.length-1]);     } }

--- Maximum_of_an_array.java
+++ Maximum_of_an_array.java
@@ -2,18 +2,12 @@ import java.util.*;
 public class Suresh{
     public static void main(String args[]){
         Scanner md = new Scanner (System.in);
-        int n = md.nextInt(),max=0;
+        int n = md.nextInt();
         int[] a = new int[n];
         for (int i=0;i<n;i++){
             a[i] = md.nextInt();
         }
-        max = a[0];
-        for (int i=0;i<n;i++){
-            
-            if(max<a[i]){
-                max=a[i];
-            }
-        }
-        System.out.println(max);
+        Arrays.sort(a);
+        System.out.println(a[a.length-1]);
     }
 }
