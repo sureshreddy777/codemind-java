6458f2730a0d2cac00480fee13228d9877715691
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-25 10:57:08 +0530

    import java.util.*;
    public class Suresh {
        public static void main(String args[]) {
            Scanner md = new Scanner(System.in);
            int n = md.nextInt();
            int[] arr = new int[n];
            
            for (int i = 0; i < n; i++) {
                arr[i] = md.nextInt();
            }
            
            int a = md.nextInt();
            int b = md.nextInt();
            
            int sum = 0;
            for (int i = 0; i < n; i++) {
                if (arr[i] < a || arr[i] > b) {
                    sum += arr[i];  
                }
            }
            System.out.println(sum);
        }
    }

---
+++ Sum_of_elements_not_between_A_and_B.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Suresh {
+    public static void main(String args[]) {
+        Scanner md = new Scanner(System.in);
+        int n = md.nextInt();
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = md.nextInt();
+        }
+        
+        int a = md.nextInt();
+        int b = md.nextInt();
+        
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < a || arr[i] > b) {
+                sum += arr[i];  
+            }
+        }
+        System.out.println(sum);
+    }
+}
