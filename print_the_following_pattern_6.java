f341c655d4792e4d5130b1a293abc81413a070a8
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-26 11:05:17 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner (System.in);         int n = md.nextInt();         for (int i=1;i<=n;i++){             for (int j=n;j>=1;--j){                 System.out.print(j+" ");             }             System.out.println();         }     } }

---
+++ print_the_following_pattern_6.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String args[]){
+        Scanner md = new Scanner (System.in);
+        int n = md.nextInt();
+        for (int i=1;i<=n;i++){
+            for (int j=n;j>=1;--j){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
