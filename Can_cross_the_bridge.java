329c1306029fe8833254461fd499abcb6c7cba40
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-27 22:28:12 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner (System.in);         int x = md.nextInt();         int y = md.nextInt();         int z = md.nextInt();         int r = (z-y)/x;         System.out.println(r);     } }

---
+++ Can_cross_the_bridge.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String args[]){
+        Scanner md = new Scanner (System.in);
+        int x = md.nextInt();
+        int y = md.nextInt();
+        int z = md.nextInt();
+        int r = (z-y)/x;
+        System.out.println(r);
+    }
+}
