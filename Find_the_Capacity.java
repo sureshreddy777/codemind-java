539bee2d4d431d2d1373302e58c4907d655480fa
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-28 12:02:56 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md  =new Scanner (System.in);         int b,t,s;         s = md.nextInt();         t = md.nextInt();         b = md.nextInt();         int c  = b*t*s;         System.out.println(c+"KB");     } }

---
+++ Find_the_Capacity.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String args[]){
+        Scanner md  =new Scanner (System.in);
+        int b,t,s;
+        s = md.nextInt();
+        t = md.nextInt();
+        b = md.nextInt();
+        int c  = b*t*s;
+        System.out.println(c+"KB");
+    }
+}
