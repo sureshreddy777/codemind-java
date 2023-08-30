4f435025fa46c39d656f2e8dd7421d9561adc43f
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-30 19:55:41 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner (System.in);         int n = md.nextInt();         int[] a = new int[n];         for (int i=0;i<n;i++){             a[i] = md.nextInt();         }         Arrays.sort(a);         for (int i:a){             System.out.print(i+" ");         }     } }

---
+++ Sort_an_Array_of_0s_and_1s.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String args[]){
+        Scanner md = new Scanner (System.in);
+        int n = md.nextInt();
+        int[] a = new int[n];
+        for (int i=0;i<n;i++){
+            a[i] = md.nextInt();
+        }
+        Arrays.sort(a);
+        for (int i:a){
+            System.out.print(i+" ");
+        }
+    }
+}
