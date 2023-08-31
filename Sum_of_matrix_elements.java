26b94140d7d1fd3902b032b460489936b8d40970
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-08-31 18:39:59 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner (System.in);         int n = md.nextInt();         int m = md.nextInt();         int[][] a = new int[n][m];         for (int i=0;i<n;i++){             for (int j=0;j<m;j++){                 a[i][j] = md.nextInt();             }         }         int sum = 0;         for (int i=0;i<n;i++){             for (int j=0;j<m;j++){                 sum += a[i][j];             }         }         System.out.println(sum);     } }

--- Sum_of_matrix_elements.java
+++ Sum_of_matrix_elements.java
@@ -2,19 +2,18 @@ import java.util.*;
 public class Suresh{
     public static void main(String args[]){
         Scanner md = new Scanner (System.in);
-        int n,m;
-        n = md.nextInt();
-        m = md.nextInt();
+        int n = md.nextInt();
+        int m = md.nextInt();
         int[][] a = new int[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for (int i=0;i<n;i++){
+            for (int j=0;j<m;j++){
                 a[i][j] = md.nextInt();
             }
         }
-        int sum=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                sum+= a[i][j];
+        int sum = 0;
+        for (int i=0;i<n;i++){
+            for (int j=0;j<m;j++){
+                sum += a[i][j];
             }
         }
         System.out.println(sum);
