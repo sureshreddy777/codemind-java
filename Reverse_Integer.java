8ed9c1a0d368557d0bd188e9a4764196f00e9c77
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-09-09 21:18:44 +0530

    import java.util.*; public class Suresh{     public static void main(String args[]){         Scanner md = new Scanner (System.in);         int n = md.nextInt();         int rev = 0,rem;         while (n != 0){             rem = n%10;             rev = rev * 10+rem;             n = n/10;         }         System.out.println(rev);     } }

---
+++ Reverse_Integer.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Suresh{
+    public static void main(String args[]){
+        Scanner md = new Scanner (System.in);
+        int n = md.nextInt();
+        int rev = 0,rem;
+        while (n != 0){
+            rem = n%10;
+            rev = rev * 10+rem;
+            n = n/10;
+        }
+        System.out.println(rev);
+    }
+}
