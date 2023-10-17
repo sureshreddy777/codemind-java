8bce35cfc4cee0a4b9abc5eea515a08788ad94ea
Suresh Reddy <119723601+sureshreddy777@users.noreply.github.com>
2023-10-17 15:18:22 +0530

    import java.util.*; public class Suresh{     public static void main(String[] asd){         Scanner sc = new Scanner(System.in);         int n= sc.nextInt();         if(n>=3 && n<=100){         for(int i=1;i<n;i++){             for(int j=1;j<=i;j++)                 System.out.print("*");             System.out.println();         }         for(int i=n;i>=1;i--){             for(int j=i;j>=1;j--)                 System.out.print("*");             System.out.println();         }         }else{             System.out.print("The pattern is not possible");         }     } }

--- Half_diamond_star_pattern.java
+++ Half_diamond_star_pattern.java
@@ -1,8 +1,8 @@
 import java.util.*;
 public class Suresh{
-    public static void main(String[] args){
-        Scanner md = new Scanner(System.in);
-        int n= md.nextInt();
+    public static void main(String[] asd){
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
         if(n>=3 && n<=100){
         for(int i=1;i<n;i++){
             for(int j=1;j<=i;j++)
